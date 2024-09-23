02915253032c191bd3dfa9c7135fb3299423ba07
Roald Ruiter <roaldruiter@gmail.com>
2024-09-23 21:10:01 +0200

    add timing

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ print("Press 'p' to capture a photo. Press 'q' to quit.")
 
 while True:
     if key_pressed("p"):
+        start_time = time.time()
         print("p pressed")
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
         filename = f"{timestamp}.jpg"
         picam2.capture_file(filename)
+
+        end_time = time.time()
+        capture_time = end_time - start_time
+        print(f"Image captured: {filename}, Time taken: {capture_time:.2f} seconds")
+
         # Add a small delay to avoid capturing multiple images with one keypress
         time.sleep(0.5)
     elif key_pressed("q"):
