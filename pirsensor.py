3401c4d6c9b22c4a896449ff817e2e6a1b7dd82a
Roald Ruiter <roaldruiter@gmail.com>
2024-10-05 15:12:00 +0200

    test light script

---
+++ pirsensor.py
@@ -0,0 +1,20 @@
+import time
+from datetime import datetime
+
+import RPi.GPIO as GPIO
+
+if __name__ == "__main__":
+    # The GPIO.BCM is how they are called on the sticker. D4 = 4 then.
+    # If you use GPIO.BOARD D4 = pin 7,
+    GPIO.setmode(GPIO.BCM)
+
+    pir_sensor_bcm_pin = 4
+    GPIO.setup(pir_sensor_bcm_pin, GPIO.IN)
+
+    while True:
+        if GPIO.input(pir_sensor_bcm_pin):
+            print(f"{datetime.now().strftime('%H:%M:s')} detected something")
+        else:
+            print("Nothing")
+
+        time.sleep(1)
